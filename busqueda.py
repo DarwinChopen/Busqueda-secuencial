5b66651cff0cc257cdf3bf336a66fcfcf8942745
Darwin <jackdarwinbot@gmail.com>
2025-08-06 11:05:31 -0600

    segundo comit, se realizo el ingreso de datos

--- busqueda.py
+++ busqueda.py
@@ -1,3 +1,4 @@
+repartidores={}
 while True:
     print("Menu")
     print("1. Ingresar")
@@ -11,8 +12,22 @@ while True:
     match opcion:
         case 1:
             print("Ingreso datos")
+            cantidad = int(input("¿Cuántos Repartidores desea ingresar? "))
+            for i in range(cantidad):
+                print(f"\nRepartidor #{i + 1}")
+                while True:
+                   codigo = input(int("Ingrese el Codigo del repartidor: "))
+                   if codigo in repartidores:
+                       print("Este codigo ya existe Ingrese otro")
+                   else:
+                       break
+                repartidores[codigo] = {}
+                repartidores[codigo]["nombre"] = input("Ingrese el nombre del repartidor: ")
+                repartidores[codigo]["cantidadPaquetes"]=int(input("Ingrese la cantidad de paquetes"))
+                repartidores[codigo]["zona"]=input("Ingrese la zona")
         case 2:
             print("Datos Ordenados")
+
         case 3:
             print("Buscar")
         case 4:
